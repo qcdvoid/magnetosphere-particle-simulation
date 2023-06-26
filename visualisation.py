1e655c8939e4a0d51734419ca2f97fd703bfae67
voi∂ <81336306+qcdvoid@users.noreply.github.com>
2023-06-26 15:53:17 +0200

    Update visualisation.py
    Comment translation

--- visualisation.py
+++ visualisation.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-## TRAJECTOIRES
+## TRAJECTORIES
 
 l = np.loadtxt('particle.dat')
 nit=l[0,0]
@@ -24,7 +24,7 @@ exz=extrema[:,2]
 fig3d = plt.figure()
 ax = fig3d.add_subplot(projection='3d')
 ax.plot(x,y,z)
-ax.set_title('Trajectoire')
+ax.set_title('Trajectory')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
@@ -34,15 +34,15 @@ fig, axs = plt.subplots(2,1)
 
 axs[0].plot(t,x)
 axs[0].set_title('x(t)')
-axs[0].set_xlabel('t (en s)')
-axs[0].set_ylabel('x (en m)')
+axs[0].set_xlabel('t (in s)')
+axs[0].set_ylabel('x (in m)')
 
 axs[1].plot(t,y)
 axs[1].set_title('y(t)')
-axs[1].set_xlabel('t (en s)')
-axs[1].set_ylabel('y (en m)')
+axs[1].set_xlabel('t (in s)')
+axs[1].set_ylabel('y (in m)')
 
-##INVARIANTS ET PÉRIODES
+##INVARIANTS AND PERIODS
 
 #invariants
 idp = np.loadtxt('inv_dip.dat')
@@ -58,19 +58,19 @@ fig2, axs2 = plt.subplots(1,2)
 
 axs2[0].plot(tdp,inv_dp)
 axs2[0].set_title(r'$\mu(t)=\frac{m{u_\perp}\!^2}{\|\vec{B}\|}$')
-axs2[0].set_xlabel('t (en s)')
-axs2[0].set_ylabel(r'$\mu$ (en C.m)')
+axs2[0].set_xlabel('t (in s)')
+axs2[0].set_ylabel(r'$\mu$ (in C.m)')
 
 avg = ilong[-1]
 avg = '{:.4e}'.format(avg)
 
 axs2[1].scatter(nbper,inv_long, label=r'$<J> = $'+str(avg))
 axs2[1].set_title(r'$J = \int_{C_1}^{C_2}u_\parallel ds$')
-axs2[1].set_xlabel('Demi-périodes')
-axs2[1].set_ylabel('J (en m^2/s)')
+axs2[1].set_xlabel('Half-periods')
+axs2[1].set_ylabel('J (in m^2/s)')
 axs2[1].legend()
 
-#periodes Tx Ty
+#periods Tx Ty
 
 Tx = np.loadtxt('Tx.dat')
 Ty = np.loadtxt('Ty.dat')
@@ -79,32 +79,32 @@ Tz = np.loadtxt('Tz.dat')
 fig3, axs3 = plt.subplots(2,1)
 
 axs3[0].plot(Tx[:-1,0],Tx[:-1,1], label='<Tx> = '+str('{:.4e}'.format(Tx[-1,0])))
-axs3[0].set_title('Tx(t), sur une période')
-axs3[0].set_xlabel('t (en s)')
-axs3[0].set_ylabel('Tx (en s)')
+axs3[0].set_title('Tx(t), over a period')
+axs3[0].set_xlabel('t (in s)')
+axs3[0].set_ylabel('Tx (in s)')
 axs3[0].legend()
 
 axs3[1].plot(Ty[:-1,0],Ty[:-1,1], label='<Ty> = '+str('{:.4e}'.format(Ty[-1,0])))
-axs3[1].set_title('Ty(t), sur une période')
-axs3[1].set_xlabel('t (en s)')
-axs3[1].set_ylabel('Ty (en s)')
+axs3[1].set_title('Ty(t), over a period')
+axs3[1].set_xlabel('t (in s)')
+axs3[1].set_ylabel('Ty (in s)')
 axs3[1].legend()
 
 
-#z(t), periode Tz
+#z(t), period Tz
 fig4, axs4 = plt.subplots(2,1)
 
 axs4[0].plot(t,z)
-axs4[0].scatter(tz,exz, marker='*', color='red', label="extremas de z(t)")
+axs4[0].scatter(tz,exz, marker='*', color='red', label="extrema of z(t)")
 axs4[0].set_title('z(t)')
-axs4[0].set_xlabel('t (en s)')
-axs4[0].set_ylabel('z (en m)')
+axs4[0].set_xlabel('t (in s)')
+axs4[0].set_ylabel('z (in m)')
 axs4[0].legend()
 
 axs4[1].scatter(Tz[:-1,0],Tz[:-1,1], label='<Tz> = '+str('{:.4e}'.format(Tz[-1,0])))
-axs4[1].set_title('Tz(t), sur une période')
-axs4[1].set_xlabel('t (en s)')
-axs4[1].set_ylabel('Tz (en s)')
+axs4[1].set_title('Tz(t), over a period')
+axs4[1].set_xlabel('t (in s)')
+axs4[1].set_ylabel('Tz (in s)')
 axs4[1].legend()
 
 plt.show()

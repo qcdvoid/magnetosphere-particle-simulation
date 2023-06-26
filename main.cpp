dff85abe4823d4d36f97e0debd7715cfb2654d1e
voi∂ <81336306+qcdvoid@users.noreply.github.com>
2023-06-26 15:15:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 /*launches all necessary files*/
 
 int main(){
-  int status = system("ccc integration.cpp; ccc prep_analyse.cpp; ccc analyse.cpp; python3 vis.py");
+  int status = system("ccc integration.cpp; ccc prep_analyse.cpp; ccc analyse.cpp; python3 visualisation.py");
   return 0;
 }
